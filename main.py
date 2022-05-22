c4895534956fb5bef2ab828dd77eeef0a609f6ae
AtesliSerap <104264328+AtesliSerap3@users.noreply.github.com>
2022-05-22 12:42:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from colorama import Fore
+
+
+def gen():
+    import random
+    import colorama
+
+    colorama.init()
+
+    upper_letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower_letter = 'abcdefghijklmnopqrstuvwxyz'
+    digits = '0123456789'
+
+    numbtogen = input('''
+ ███▄    █  ██▓▄▄▄█████▓ ██▀███   ▒█████       ▄████ ▓█████  ███▄    █ 
+ ██ ▀█   █ ▓██▒▓  ██▒ ▓▒▓██ ▒ ██▒▒██▒  ██▒    ██▒ ▀█▒▓█   ▀  ██ ▀█   █ 
+▓██  ▀█ ██▒▒██▒▒ ▓██░ ▒░▓██ ░▄█ ▒▒██░  ██▒   ▒██░▄▄▄░▒███   ▓██  ▀█ ██▒
+▓██▒  ▐▌██▒░██░░ ▓██▓ ░ ▒██▀▀█▄  ▒██   ██░   ░▓█  ██▓▒▓█  ▄ ▓██▒  ▐▌██▒
+▒██░   ▓██░░██░  ▒██▒ ░ ░██▓ ▒██▒░ ████▓▒░   ░▒▓███▀▒░▒████▒▒██░   ▓██░
+░ ▒░   ▒ ▒ ░▓    ▒ ░░   ░ ▒▓ ░▒▓░░ ▒░▒░▒░     ░▒   ▒ ░░ ▒░ ░░ ▒░   ▒ ▒ 
+░ ░░   ░ ▒░ ▒ ░    ░      ░▒ ░ ▒░  ░ ▒ ▒░      ░   ░  ░ ░  ░░ ░░   ░ ▒░
+   ░   ░ ░  ▒ ░  ░        ░░   ░ ░ ░ ░ ▒     ░ ░   ░    ░      ░   ░ ░ 
+         ░  ░              ░         ░ ░           ░    ░  ░         ░ 
+by serap puro#9070                                                          
+
+How Much I Generate :                                                         
+    ''')
+    upper, lower, nums, = True, True, True,
+    all = ""
+
+    if upper:
+        all += upper_letter
+    if lower:
+        all += lower_letter
+    if nums:
+        all += digits
+
+        lenght = 23
+        amount = numbtogen
+
+        for x in range(int(numbtogen)):
+            nitro = ''.join(random.sample(all, lenght))
+            print(Fore.BLUE + 'discord.gift/'+nitro)
+
+gen()
